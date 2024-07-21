fbde5008b93180049b1b5b8fb98be867e4cebf0d
parthk02 <parthsunilkashyap@gmail.com>
2024-07-22 00:49:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
